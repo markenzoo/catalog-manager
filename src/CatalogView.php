776dac5e3a7ca428422a86ed541a7e9f639815fd
Falk-N <falk.natkowski@web.de>
2025-09-16 14:34:21 +0200

    Fix PHP 8 Warning: Undefined array key "" in CatalogView.php

--- src/CatalogView.php
+++ src/CatalogView.php
@@ -642,7 +642,10 @@ class CatalogView extends CatalogController
                 $objResponseContext = System::getContainer()->get('contao.routing.response_context_accessor')->getResponseContext();
                 $objHeadBag = $objResponseContext->get(HtmlHeadBag::class);
 
-                $strMetaTitle = \strip_tags($arrCatalog[$this->catalogSEOTitle ?? '']);
+                if (!empty($this->catalogSEOTitle) && isset($arrCatalog[$this->catalogSEOTitle])) {
+                    $strMetaTitle = strip_tags($arrCatalog[$this->catalogSEOTitle]);
+                }
+                //$strMetaTitle = \strip_tags($arrCatalog[$this->catalogSEOTitle ?? '']);
                 $strMetaDescription = \strip_tags($arrCatalog[$this->catalogSEODescription ?: '']);
 
                 $GLOBALS['objPage']->pageTitle = $strMetaTitle ?: $objPage->pageTitle;
