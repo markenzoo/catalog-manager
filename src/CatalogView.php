ffc09ff988832350fe939f79ad604ccb5a98afef
Falk-N <falk.natkowski@web.de>
2025-09-16 14:36:27 +0200

    Fix PHP 8 Warning: Undefined array key "" in CatalogView.php

--- src/CatalogView.php
+++ src/CatalogView.php
@@ -642,11 +642,15 @@ class CatalogView extends CatalogController
                 $objResponseContext = System::getContainer()->get('contao.routing.response_context_accessor')->getResponseContext();
                 $objHeadBag = $objResponseContext->get(HtmlHeadBag::class);
 
-                if (!empty($this->catalogSEOTitle) && isset($arrCatalog[$this->catalogSEOTitle])) {
-                    $strMetaTitle = strip_tags($arrCatalog[$this->catalogSEOTitle]);
-                }
                 //$strMetaTitle = \strip_tags($arrCatalog[$this->catalogSEOTitle ?? '']);
-                $strMetaDescription = \strip_tags($arrCatalog[$this->catalogSEODescription ?: '']);
+                if (!empty($this->catalogSEOTitle) && isset($arrCatalog[$this->catalogSEOTitle])) {
+                    $strMetaTitle = \strip_tags($arrCatalog[$this->catalogSEOTitle]);
+                }
+                
+                //$strMetaDescription = \strip_tags($arrCatalog[$this->catalogSEODescription ?: '']);
+                if (!empty($this->catalogSEODescription) && isset($arrCatalog[$this->catalogSEODescription])) {
+                    $strMetaDescription = \strip_tags($arrCatalog[$this->catalogSEODescription]);
+                }
 
                 $GLOBALS['objPage']->pageTitle = $strMetaTitle ?: $objPage->pageTitle;
                 $GLOBALS['objPage']->description = $strMetaDescription ?: $objPage->description;
